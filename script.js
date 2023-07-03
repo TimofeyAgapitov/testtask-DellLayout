99674be3e5bab6749e7979bb68e023c217d3d0b9
_tmf <67283452+TimofeyAgapitov@users.noreply.github.com>
2023-07-03 12:11:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const checkbox = document.getElementById('burger-toggle');
-const body = document.body;
-const html = document.documentElement;
+const checkbox = document.getElementById('burger-toggle'),
+  body = document.body,
+  html = document.documentElement,
+  form = document.getElementById('form'),
+  nameInput = document.getElementById('name_input'),
+  phoneInput = document.getElementById('phone_input');
 
 checkbox.addEventListener('change', function () {
   if (this.checked) {
@@ -11,3 +14,35 @@ checkbox.addEventListener('change', function () {
     html.classList.remove('no-scroll');
   }
 });
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  name = nameInput.value.trim();
+  phone = phoneInput.value.trim();
+
+  if (name === '') {
+    alert('Пожалуйста, введите ваше имя');
+    nameInput.focus();
+    return;
+  }
+
+  if (phone === '') {
+    alert('Пожалуйста, введите номер телефона');
+    phoneInput.focus();
+    return;
+  }
+
+  if (!isValidPhone(phone)) {
+    alert('Пожалуйста, введите корректный номер телефона');
+    phoneInput.focus();
+    return;
+  }
+
+  this.submit();
+});
+
+function isValidPhone(phone) {
+  var phoneRegex = /^\d{10}$/;
+  return phoneRegex.test(phone);
+}
